e718a65792541b9b6e17440d404f68f768666849
DLHTX <949729789@qq.com>
2022-11-10 13:32:05 +0800

    fix:output watch

--- assembly/index.ts
+++ assembly/index.ts
@@ -7,7 +7,6 @@ declare function ws_set_db(key: string, key_size: u32, v: i32): void
 @external("env", "ws_get_db")
 declare function ws_get_db(key: string, key_size: u32): i32
 
-
 export function start(rid: i32): i32 {
     const message = "Hello";
     ws_log(message, message.length);
